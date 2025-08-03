6b243f6746a600b751ac0d418735e1413575809d
hyuzenn <huj22641@naver.com>
2025-08-03 02:34:54 -0400

    map_server_launch.py 생성

---
+++ v2x_ball_bot_bringup/launch/map_server_launch.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{
+                'yaml_filename': '/root/map/my_map.yaml'  
+            }]
+        )
+    ])
